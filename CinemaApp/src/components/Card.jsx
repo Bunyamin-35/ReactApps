f1d8aaa1fc8f60b084ab8f53745327eb3ed6db2c
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-06-16 12:30:02 +0300

    CinemaApp | Card Content modified.

--- CinemaApp/src/components/Card.jsx
+++ CinemaApp/src/components/Card.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import styles from "./card.module.scss"
+import { Link } from 'react-router-dom';
+
+import {
+    StarOutlined,
+    LikeOutlined,
+    CaretRightOutlined,
+} from '@ant-design/icons';
 
 const Card = ({ films }) => {
     return (
@@ -10,8 +17,21 @@ const Card = ({ films }) => {
                         <div className={styles.card_image}>
                             <img src={item.image.original}></img>
                         </div>
-                        <div className={styles.card_info}>
-                            {item.name}
+                        <div className={styles.card_content}>
+                            <div className={styles.rating}>
+                                <span className={styles.rating_icon}><StarOutlined /></span>
+                                <span className={styles.average_rating}>{item.rating.average}</span>
+                                <button className={styles.rate}><span><LikeOutlined /></span></button>
+                            </div>
+                            <div className={styles.film_info}>
+                                <h2 className={styles.film_name}><CaretRightOutlined />{item.name}</h2>
+                                <button className={styles.film_details}>
+                                    <a className={styles.detail_btn} to={`/${item.name}`}>Details</a>
+                                    <svg strokeWidth="4" stroke="currentColor" viewBox="0 0 24 24" fill="none" className={`${styles.h_6} ${styles.w_6}`} xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M14 5l7 7m0 0l-7 7m7-7H3" strokeLinejoin="round" strokeLinecap="round"></path>
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 )
