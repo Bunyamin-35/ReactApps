e98b9e57fbd490d563b4d013b73ce1fb59de7b13
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-06-16 10:13:08 +0300

    CinemaApp | Responsive design implemented to Card component.

--- CinemaApp/src/components/Card.jsx
+++ CinemaApp/src/components/Card.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import styles from "./card.module.scss"
 
-const Card = ({films}) => {
-  return (
-    <div className={styles.card}>
-        {films.map((item,index) =>{
-            return(
-                <div className={styles.card_wrapper} key={index}>
-                    <img src={item.image.original} alt='image'/>
-                    <div className={styles.card_info}>
-                        {item.name}
+const Card = ({ films }) => {
+    return (
+        <div className={styles.card_wrapper}>
+            {films.map((item, index) => {
+                return (
+                    <div className={styles.card} key={index}>
+                        <div className={styles.card_image}>
+                            <img src={item.image.original}></img>
+                        </div>
+                        <div className={styles.card_info}>
+                            {item.name}
+                        </div>
                     </div>
-                </div>
-            )
-            
-        })}
-    </div>
-  )
+                )
+
+            })}
+        </div>
+    )
 }
 
 export default Card;
