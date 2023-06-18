7cd2e57a1d23a3dbfbf74e79609e9959c5332e4d
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-06-18 13:07:29 +0300

    CinemaApp | useNavigate hook used in NotFound page.

--- CinemaApp/src/Pages/NotFound.jsx
+++ CinemaApp/src/Pages/NotFound.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  useEffect(()=>{
+    setTimeout(()=>{
+      navigate("/");
+    },1000)
+  },[])
   return (
     <div>NotFound</div>
   )
