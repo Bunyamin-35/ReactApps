0b7d1ee44d6f848ec2fccff221412f799522f3e3
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-06-24 16:09:44 +0300

    CinemaApp| Hero component modified for next and prev button onClick events.

--- CinemaApp/src/components/Hero.jsx
+++ CinemaApp/src/components/Hero.jsx
@@ -14,28 +14,50 @@ const Hero = () => {
     const { highrated, actionsShows } = useShows();
     const [showsPerPage] = useState(4);
 
-    const [startIdx, setStartIndex] = useState(0)
-    const [endIdx, setEndIdx] = useState(showsPerPage)
+    const [startIdxPopular, setStartIndexPopular] = useState(0)
+    const [endIdxPopular, setEndIdxPopular] = useState(showsPerPage)
 
-    const handleNextIndex = () => {
-        setStartIndex(startIdx + 1)
-        setEndIdx(endIdx + 1);
-        if (endIdx === highrated.length) {
-            setEndIdx(showsPerPage);
-            setStartIndex(0)
+    const [startIdxAction, setStartIndexAction] = useState(0)
+    const [endIdxAction, setEndIdxAction] = useState(showsPerPage)
+
+    
+    const handleNextIndexPopular = () => {
+        setStartIndexPopular(startIdxPopular + 1)
+        setEndIdxPopular(endIdxPopular + 1);
+        if (endIdxPopular === highrated.length) {
+            setEndIdxPopular(showsPerPage);
+            setStartIndexPopular(0)
         }
     };
-    const handlePrevIndex = () => {
-        setStartIndex(startIdx - 1)
-        setEndIdx(endIdx - 1)
-        if (startIdx === 0) {
-            setStartIndex(highrated.length - showsPerPage)
-            setEndIdx(highrated.length);
+    
+    const handlePrevIndexPopular = () => {
+        setStartIndexPopular(startIdxPopular - 1)
+        setEndIdxPopular(endIdxPopular - 1)
+        if (startIdxPopular === 0) {
+            setStartIndexPopular(highrated.length - showsPerPage)
+            setEndIdxPopular(highrated.length);
+        }
+    };
+    const handleNextIndexAction = () => {
+        setStartIndexAction(startIdxAction + 1)
+        setEndIdxAction(endIdxAction + 1);
+        if (endIdxAction === highrated.length) {
+            setEndIdxAction(showsPerPage);
+            setStartIndexAction(0)
+        }
+    };
+    
+    const handlePrevIndexAction = () => {
+        setStartIndexAction(startIdxAction - 1)
+        setEndIdxAction(endIdxAction - 1)
+        if (startIdxAction === 0) {
+            setStartIndexAction(highrated.length - showsPerPage)
+            setEndIdxAction(highrated.length);
         }
     };
 
-    const visibleShows = highrated.slice(startIdx, endIdx);
-    const visibleActionShows = actionsShows.slice(startIdx, endIdx);
+    const visibleShows = highrated.slice(startIdxPopular, endIdxPopular);
+    const visibleActionShows = actionsShows.slice(startIdxAction, endIdxAction);
 
 
     return (
@@ -48,7 +70,7 @@ const Hero = () => {
             </div>
             <div className={styles.title}><h2><span>Most</span> Popular</h2></div>
             <div className={styles.container}>
-                <button className={styles.prev_btn} onClick={handlePrevIndex}>
+                <button className={styles.prev_btn} onClick={handlePrevIndexPopular}>
                     <CaretLeftOutlined />
                 </button>
                 <div className={styles.slider}>
@@ -58,13 +80,13 @@ const Hero = () => {
                         </Link>
                     ))}
                 </div>
-                <button className={styles.next_btn} onClick={handleNextIndex}>
+                <button className={styles.next_btn} onClick={handleNextIndexPopular} >
                     <CaretRightOutlined />
                 </button>
             </div>
             <div className={styles.title}><h2><span>Action</span> Shows</h2></div>
             <div className={styles.container}>
-                <button className={styles.prev_btn} onClick={handlePrevIndex}>
+                <button className={styles.prev_btn} onClick={handlePrevIndexAction}>
                     <CaretLeftOutlined />
                 </button>
                 <div className={styles.slider}>
@@ -74,7 +96,7 @@ const Hero = () => {
                         </Link>
                     ))}
                 </div>
-                <button className={styles.next_btn} onClick={handleNextIndex}>
+                <button className={styles.next_btn} onClick={handleNextIndexAction}>
                     <CaretRightOutlined />
                 </button>
             </div>
