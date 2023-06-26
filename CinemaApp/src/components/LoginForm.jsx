f34d39ef58cb9e4e18fee190711dd22dad8ce47a
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-06-26 22:33:48 +0300

    CinemaApp|LoginForm page modified.

--- CinemaApp/src/components/LoginForm.jsx
+++ CinemaApp/src/components/LoginForm.jsx
@@ -1,85 +1,58 @@
 import React from 'react'
 
-import styles from "../app.module.scss"
+import styles from "./login.module.scss"
 
-import { Button, Checkbox, Form, Input } from 'antd';
+import {
+    GoogleOutlined,
+    GithubOutlined,
+    LinkedinOutlined,
+} from '@ant-design/icons';
 
-const onFinish = (values) => {
-    console.log('Success:', values);
-};
-const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-};
+
 
 const LoginForm = () => {
     return (
         <div className={styles.loginform}>
-            <Form
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                style={{
-                    maxWidth: 600,
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your username!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
+            <form className={styles.form}>
+                <h2>Log In</h2>
 
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
+                <div className={styles.email}>
+                    <label>Your E-mail</label>
+                    <input></input>
+                </div>
+                <div className={styles.password}>
+                    <label>Your Password</label>
+                    <input></input>
+                </div>
+                <div className={styles.alerts}>
+                    <input type='checkbox' />
+                    <label>Remember Me</label>
+                    <button>Forgot Your Password?</button>
+                </div>
+                <div className={styles.loginbtn}>
+                    <button>Log In</button>
+                </div>
+                <div className={styles.registerbtn}>
+                    <label>Not a member?</label>
+                    <button>Register</button>
+                </div>
+                <h3>Or Login with</h3> 
+                <div className={styles.loginwith}>
+                    
+                    <div>
+                        <GoogleOutlined className={styles.google} />
+                    </div>
+                    <div>
+                        <GithubOutlined className={styles.github} />
+                    </div>
+                    <div>
+                        <LinkedinOutlined className={styles.linkedin} />
+                    </div>
+                </div>
+                <div className={styles.footer}>
+                    @2023 all rights reserved
+                </div>
+            </form>
         </div>
     );
 
